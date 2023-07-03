22d4d4d9274ea892c14a562cf0f3494ec1219930
EthanLDot <ethanlee2337@gmail.com>
2023-07-02 21:51:46 -0500

    added some basic js

---
+++ FinalProject.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+    let element = document.getElementById('img0');
+    console.log(element);
+    element.src = "https://outlookvalleysun.outlooknewspapers.com/wp-content/uploads/sites/6/2023/05/Baseball_LCF-Flintridge-Prep-Team-Photo.jpg";
+  };
